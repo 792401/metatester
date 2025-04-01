c7a08d6660e236702c6330305c533ef1de284923
m <mihaidmuresan@gmail.com>
2025-04-01 17:36:31 +0300

    Added mocked integration tests and Wiremock mappings

---
+++ lib/src/test/java/metatester/integration/mockrestapi/UsersTest.java
@@ -0,0 +1,52 @@
+package metatester.integration.mockrestapi;
+
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+import io.restassured.response.Response;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+
+public class UsersTest {
+
+    @BeforeAll
+    public static void beforeAll(){
+        RestAssured.baseURI = "http://localhost:8089";
+    }
+
+    @Test
+    public void testGetUsers() {
+        Response response = given()
+                .when()
+                .get("/users");
+
+        Assertions.assertTrue(response.body().asString().contains("name"),"check name field");
+    }
+
+    @Test
+    public void testGetUser() {
+        Response response = given()
+                .when()
+                .get("/users/1");
+
+        Assertions.assertTrue(response.body().asString().contains("name"),"check name field");
+    }
+
+    @Test
+    public void testPostUsers() {
+        String requestBody = "{\"name\": \"Simulated New User\", \"email\": \"simulated.new.user@example.com\"}";
+
+        Response response = given()
+                .contentType(ContentType.JSON)
+                .body(requestBody)
+                .when()
+                .get("/users");
+
+
+        Assertions.assertTrue(response.body().asString().contains("email"),"check title field");
+    }
+
+
+}
